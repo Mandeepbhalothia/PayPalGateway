9f1ec38786d4a3f250c01caf4ee9961db231928a
Mandeepbhalothia <bhalothiamandeep@gmail.com>
2018-08-30 23:40:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/paypalgateway/PayPalConfig.java
@@ -0,0 +1,6 @@
+package com.example.user.paypalgateway;
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "AWlHHI3tnAPtKT_NKvwzcfnk5F449uHYibi6VzB5ININWUftflRGSLweXvokFTOqpyQaK0lSdP7Td5vZ";
+}
